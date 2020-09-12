192c5a873caf260e9dbf7e440ef3106ff5da7cda
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2020-09-12 13:37:28 +0200

    Lesson on control flow

--- src/main.rs
+++ src/main.rs
@@ -116,3 +116,75 @@ fn five() -> i32 {
 fn plus_one(x: i32) -> i32 {
     x + 1 // or return x + 1;
 }
+
+fn control_flow() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    // Because if is an expression, we can use it on the right side of a let statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is {}", number);
+
+    let mut count = 0;
+    // This loop is forever
+    loop {
+        println!("again!");
+
+        // guard
+        if count == 10 {
+            break;
+        }
+        count += 1;
+    }
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // while loop
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // iterate over array's elements
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
+    }
+
+    // more coincise alternatives is for loop (best performance also)
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    // loop n times with for loop use Range type
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
